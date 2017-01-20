1421cac0e6f0a15ac141538697bc0f6fbe34be3b
NStrike <davi.caldas.2007@gmail.com>
2017-01-20 12:42:59 -0200

    Add files via upload

--- NStrike Industries/src/Empregado.java
+++ NStrike Industries/src/Empregado.java
@@ -5,7 +5,7 @@ public class Empregado {
 	public int tipo; // Horista Assalariado Comissionado
 	public int metodo; // Cheque Via Correios Cheque Em Mãos Depósito Bancário
 	public int sindicato; // Sim ou Não
-	public int sindicatoID; // Para Localização da Taxa Sindical
+	public int sindicatoID; // Para Localização da Taxa Sindical	
 	
 	//Atributos
 	public float salarioFixo; // Mensal Horario
--- NStrike Industries/src/NStrikeIndustries.java
+++ NStrike Industries/src/NStrikeIndustries.java
@@ -35,11 +35,9 @@ public class NStrikeIndustries {
 			
         	Empregados[i] = new Empregado();
         	Empregados[i] = inicializar(Empregados[i]);
-        }
-		
-		i = 0;
+        }		
 
-		int Comando = 1, undo, redo;
+		int Comando = 1, undo, redo, totalDeFuncionarios = 0, id = 0;
 		
 		while(Comando > 0){
 			System.out.println("\nComandos Dísponiveis No Programa:");
@@ -60,87 +58,114 @@ public class NStrikeIndustries {
 			
 			
 			if(Comando == 1){
-				System.out.println("Iniciando Cadastro...");
-				System.out.println("\nInsira Os Dados Do Novo Funcionário:");
-				System.out.println("Obs.: Não Insira Acentos e Não Haverá Distição Entre Letras Maius. e Minus.\n"
-						+ "\nNome:");
-				Empregados[i].nome = scan.nextLine();
-				Cache.nome = Empregados[i].nome;
 				
-				System.out.println("Endereço:");
-				Empregados[i].endereco = scan.nextLine();
-				Cache.endereco = Empregados[i].endereco;
-				
-				System.out.println("Tipo De Funcionário:\n"
-						+ "\t1 - Horista\n"
-						+ "\t2 - Assalariado\n"
-						+ "\t3 - Comissionado");
-				Empregados[i].tipo = scan.nextInt();
-				scan.nextLine();
-				Cache.tipo = Empregados[i].tipo;
-				
-				System.out.println("Digite O Valor Fixo Do Salário Do Funcionário:");
-				Empregados[i].salarioFixo = scan.nextInt();
-				scan.nextLine();
-				Cache.salarioFixo = Empregados[i].salarioFixo;
-				
-				if(Empregados[i].tipo == 3){
-						System.out.println("Digite O Percentual De Comissão Sobre As Vendas Realizadas:");
-						Empregados[i].comissao = scan.nextInt();
-						scan.nextLine();
-						Cache.comissao = Empregados[i].comissao;
+				if(totalDeFuncionarios == 100){
+					System.out.println("Impossível Realizar Cadastro!\n"
+							+ "Numero Limite De Vagas Foi Atingido");
 				}
 				
-				System.out.println("Método De Pagamento:\n"
-						+ "\t1 - Cheque Via Correios\n"
-						+ "\t2 - Cheque Em Mãos\n"
-						+ "\t3 - Déposito Bancário");
-				Empregados[i].metodo = scan.nextInt();
-				scan.nextLine();
-				Cache.metodo = Empregados[i].metodo;
-				
-				System.out.println("Funcionário Pertence Ao Sindicato?\n"
-						+ "\t1 - Sim\n"
-						+ "\t2 - Não");
-				Empregados[i].sindicato = scan.nextInt();
-				scan.nextLine();
-				Cache.sindicato = Empregados[i].sindicato;
-				
-				if(Empregados[i].sindicato == 1){
-
-					System.out.println("Digite o ID Do Funcionário No Sindicato:");
-					Empregados[i].sindicatoID = scan.nextInt();
+				else{
+					
+					totalDeFuncionarios++;
+					i = 0;					
+					while(Empregados[i].nome != null){
+						i++;
+					}
+					
+					System.out.println("Iniciando Cadastro...");
+					System.out.println("\nInsira Os Dados Do Novo Funcionário:");
+					System.out.println("Obs.: Não Insira Acentos e Não Haverá Distição Entre Letras Maius. e Minus.\n"
+							+ "\nNome:");
+					Empregados[i].nome = scan.nextLine();
+					Cache.nome = Empregados[i].nome;
+					
+					System.out.println("Endereço:");
+					Empregados[i].endereco = scan.nextLine();
+					Cache.endereco = Empregados[i].endereco;
+					
+					System.out.println("Tipo De Funcionário:\n"
+							+ "\t1 - Horista\n"
+							+ "\t2 - Assalariado\n"
+							+ "\t3 - Comissionado");
+					Empregados[i].tipo = scan.nextInt();
 					scan.nextLine();
-					Cache.sindicatoID = Empregados[i].sindicatoID;					
+					Cache.tipo = Empregados[i].tipo;
+					
+					System.out.println("Digite O Valor Fixo Do Salário Do Funcionário:");
+					Empregados[i].salarioFixo = scan.nextInt();
+					scan.nextLine();
+					Cache.salarioFixo = Empregados[i].salarioFixo;
+					
+					if(Empregados[i].tipo == 3){
+							System.out.println("Digite O Percentual De Comissão Sobre As Vendas Realizadas:");
+							Empregados[i].comissao = scan.nextInt();
+							scan.nextLine();
+							Cache.comissao = Empregados[i].comissao;
+					}
+					
+					System.out.println("Método De Pagamento:\n"
+							+ "\t1 - Cheque Via Correios\n"
+							+ "\t2 - Cheque Em Mãos\n"
+							+ "\t3 - Déposito Bancário");
+					Empregados[i].metodo = scan.nextInt();
+					scan.nextLine();
+					Cache.metodo = Empregados[i].metodo;
+					
+					System.out.println("Funcionário Pertence Ao Sindicato?\n"
+							+ "\t1 - Sim\n"
+							+ "\t2 - Não");
+					Empregados[i].sindicato = scan.nextInt();
+					scan.nextLine();
+					Cache.sindicato = Empregados[i].sindicato;
+					
+					if(Empregados[i].sindicato == 1){
+
+						System.out.println("Digite o ID Do Funcionário No Sindicato:");
+						Empregados[i].sindicatoID = scan.nextInt();
+						scan.nextLine();
+						Cache.sindicatoID = Empregados[i].sindicatoID;					
+					}
+					
+					System.out.printf("Funcionário %s Cadastrado Com Sucesso!\n", Empregados[i].nome);
+					System.out.printf("Numero De Indentificação %02d \n\n", i);
 				}
 			}
 			
 			else if(Comando == 2){
 				
-				int id, confirmar = 2;
+				int confirmar = 2;
 				while(confirmar == 2){
 					
 					System.out.println("Digite o ID Do Funcionário Que Deseja Remover Do Sistema:");
 					id = scan.nextInt();
 					scan.nextLine();
 					
-					System.out.printf("Deseja Remover O Funcionário %s?\n"
-							+ "\t1 - Sim\n"
-							+ "\t2 - Escolher Outro Funcionário\n", Empregados[id].nome);
-					confirmar = scan.nextInt();
-					scan.nextLine();
-					
-					if(confirmar == 1){
-						Cache.nome = Empregados[id].nome;
-						Cache.endereco = Empregados[id].endereco;
-						Cache.tipo = Empregados[id].tipo;
-						Cache.salarioFixo = Empregados[id].salarioFixo;
-						Cache.comissao = Empregados[id].comissao;
-						Cache.metodo = Empregados[id].metodo;
-						Cache.sindicato = Empregados[id].sindicato;
-						Cache.sindicatoID = Empregados[id].sindicatoID;
-						Empregados[id] = inicializar(Empregados[id]);
+					if(Empregados[id].nome == null){
+						System.out.printf("Não Existe Nenhum Cadastro De Funcinário Com O ID(%d)\n");
 					}
+					
+					else{
+						
+						System.out.printf("Deseja Remover O Funcionário %s?\n"
+								+ "\t1 - Sim\n"
+								+ "\t2 - Escolher Outro Funcionário\n", Empregados[id].nome);
+						confirmar = scan.nextInt();
+						scan.nextLine();
+						
+						if(confirmar == 1){
+							totalDeFuncionarios--;
+							Cache.nome = Empregados[id].nome;
+							Cache.endereco = Empregados[id].endereco;
+							Cache.tipo = Empregados[id].tipo;
+							Cache.salarioFixo = Empregados[id].salarioFixo;
+							Cache.comissao = Empregados[id].comissao;
+							Cache.metodo = Empregados[id].metodo;
+							Cache.sindicato = Empregados[id].sindicato;
+							Cache.sindicatoID = Empregados[id].sindicatoID;
+							Empregados[id] = inicializar(Empregados[id]);
+							System.out.printf("Funcionário %s Removido Com Sucesso!\n", Cache.nome);
+						}
+					}	
 				}				
 			}
 			
@@ -166,16 +191,18 @@ public class NStrikeIndustries {
 						
 					case 1:
 						Empregados[i] = inicializar(Empregados[i]);
+						totalDeFuncionarios--;
 						
 					case 2:
-						Empregados[i].nome = Cache.nome;
-						Empregados[i].endereco = Cache.endereco;
-						Empregados[i].tipo = Cache.tipo;
-						Empregados[i].salarioFixo = Cache.salarioFixo;
-						Empregados[i].comissao = Cache.comissao;
-						Empregados[i].metodo = Cache.metodo;
-						Empregados[i].sindicato = Cache.sindicato;
-						Empregados[i].sindicatoID = Cache.sindicatoID;
+						totalDeFuncionarios++;
+						Empregados[id].nome = Cache.nome;
+						Empregados[id].endereco = Cache.endereco;
+						Empregados[id].tipo = Cache.tipo;
+						Empregados[id].salarioFixo = Cache.salarioFixo;
+						Empregados[id].comissao = Cache.comissao;
+						Empregados[id].metodo = Cache.metodo;
+						Empregados[id].sindicato = Cache.sindicato;
+						Empregados[id].sindicatoID = Cache.sindicatoID;
 					}					
 					
 					System.out.println("Deseja Refazer A Última Ação?");
@@ -187,6 +214,7 @@ public class NStrikeIndustries {
 						switch(Comando){
 						
 						case 1:
+							totalDeFuncionarios++;
 							Empregados[i].nome = Cache.nome;
 							Empregados[i].endereco = Cache.endereco;
 							Empregados[i].tipo = Cache.tipo;
@@ -197,7 +225,8 @@ public class NStrikeIndustries {
 							Empregados[i].sindicatoID = Cache.sindicatoID;
 							
 						case 2:
-							Empregados[i] = inicializar(Empregados[i]);
+							Empregados[id] = inicializar(Empregados[id]);
+							totalDeFuncionarios--;
 						}
 						
 					}
